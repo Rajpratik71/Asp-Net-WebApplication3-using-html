0c0598c3d10b4b8a3bba538467cb8172e9a5d246
Pratik raj <rajratik71@gmail.com>
2017-09-09 08:09:38 -0700

    Used user defined controller "user" instead of default controller "Home".
    Steps to create user controllers are :
    
    1. In solution explorer , right click on controllers then select "add" then choose "Controllers" add.
    
    2. Further in "UserController.cs" , right click on "Index()" and then choose "add view" , name that as "index" , then done.
    
    3. Now , in "index.cshtml" , write down your normal html code , done.
    
    4. Now , in "Solution Explorer" choose "app_start" in "routeconfig.cs" , change "controller = Home" to "controller = user " .
    
    5 . Finally all done. Now run the application

--- Asp-Net-WebApplication3-using-html/App_Start/RouteConfig.cs
+++ Asp-Net-WebApplication3-using-html/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Asp_Net_WebApplication3_using_html
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "user", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
